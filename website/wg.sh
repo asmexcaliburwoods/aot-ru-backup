eba02730a89575cc4cdfcb0485923a3fdc141a61
user <user>
2020-01-20 09:29:23 +0800

    fetched the website

---
+++ website/wg.sh
@@ -0,0 +1,1 @@
+wget -t0 -k -c -r -np -l0 -o wg.log http://aot.ru/ > stdout.log 2>&1 &
---
+++ website/wg2.sh
@@ -0,0 +1,1 @@
+wget -X /demo,/search1.html -t0 -k -c -r -np -l0 -o wg2.log http://aot.ru/ > stdout2.log 2>&1 &
